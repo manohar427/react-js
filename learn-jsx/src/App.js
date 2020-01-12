799280752963f55843ac67e22e50b003d654ee08
Manohar Sambayyapalem <manohar427@gmail.com>
2020-01-12 21:28:26 +0530

    React js tutorial

---
+++ learn-jsx/src/App.js
@@ -0,0 +1,21 @@
+import React,{Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Bbb(){
+  return <h1>Example1</h1>
+}
+
+class App extends Component{
+
+ render(){
+   return (
+
+     <div>
+         <h1>JJJJJ</h1>
+     </div>
+   )
+ }
+}
+
+export default App;
