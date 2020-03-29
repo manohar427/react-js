87fc1f8083e63e90486d258d85e63c9bdb98d9fc
Manohar S <manohar427@gmail.com>
2020-03-29 10:07:42 +0530

    React example jest-000

--- test-10/src/App.js
+++ test-10/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+class  App extends React.Component {
+  render(){
+    axios.get("http://demo-my-test.2886795366-80-simba02.environments.katacoda.com/").then(d=>{
+      console.log(d);
+    });
   return (
     <div className="App">
      Hi Manohar s
     </div>
   );
+  }
 }
 
 export default App;
