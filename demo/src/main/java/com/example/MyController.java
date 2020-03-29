1695a32e15feae22ca31a44c6086b748f607db69
Manohar S <manohar427@gmail.com>
2020-03-29 09:48:48 +0530

    Rest Example-999

--- demo/src/main/java/com/example/MyController.java
+++ demo/src/main/java/com/example/MyController.java
@@ -1,14 +1,12 @@
 package com.example;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("\test")
 public class MyController {
 
-	@GetMapping("abc")
+	@RequestMapping("/")
 	public String getName() {
 		return "Manohar";
 	}
