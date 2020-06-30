d73b2b522afbba55634f558bf232d01c3225d624
Manohar S <manohar427@gmail.com>
2020-06-30 12:42:00 +0530

    React csv download example

--- csv-download/src/App.js
+++ csv-download/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
-import { CSVLink } from "react-csv";
+import { CSVLink,CSVDownload } from "react-csv";
+import axios from 'axios';
  
-const csvData = [
+const data = [
   ["FirstName", "Last Name", "Email Test"],
   ["Ahmed", "Tomi", "ah@smthing.co.com"],
   ["Raed", "Labes", "rl@smthing.co.com"],
@@ -10,13 +11,59 @@ const csvData = [
 ];
 
 
-function App() {
-  let filename = "Downoad"+new Date().toString()+".csv"
-  return (
-    <div className="App">
-      <CSVLink data={csvData} target="_blank" className="btn btn-success" filename={filename}>Download me</CSVLink>
-    </div>
-  );
+//let csvLinkRef = React.createRef();
+class App extends React.Component{
+  state = {
+    flag:false
+  }
+
+  
+  constructor(props) {
+    super(props);
+
+    //this.textInput = null;
+    this.csvClick = null;
+
+    /*
+    this.setTextInputRef = element => {
+      this.textInput = element;
+    };
+    */
+    //setcsv
+    this.setcsv = element => {
+      this.csvClick = element;
+    };
+
+    this.focusTextInput = () => {
+      // Focus the text input using the raw DOM API
+      if (this.textInput) 
+      this.textInput.focus();
+
+    if (this.csvClick) 
+        this.csvClick.link.click();
+    };
+  }
+
+  focusTextInput() {
+   
+  }
+ 
+  render() {
+
+ return (<>
+  <CSVLink data={data} filename="test.csv"  ref={this.setcsv} target="_self" >OK</CSVLink>
+  <br/>
+
+        <input
+          type="button"
+          value="Focus the text input"
+          onClick={this.focusTextInput}
+        />
+ 
+ </>)
+  }
+  
 }
+
 
 export default App;
